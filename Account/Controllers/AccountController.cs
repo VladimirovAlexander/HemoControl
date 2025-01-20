e0143d370c9b80398b5bf60960de6afb1a5c3715
Александр Владимиров <a.v.vladimirov111@gmail.com>
2025-01-20 15:04:55 +0300

    Добавит контроллер AccountController

---
+++ Account/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Account.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController:Controller
+    {
+        [HttpPost("register")]
+
+        public async Task<IActionResult> Register()
+        {
+
+        }
+    }
+}
