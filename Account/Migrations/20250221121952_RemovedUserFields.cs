ac347ced484514a3f1fee36a421a5f2da4fef6db
Александр Владимиров <a.v.vladimirov111@gmail.com>
2025-02-21 15:33:13 +0300

    Исправит поля таблицы users, добавит миграцию RemovedUserField

---
+++ Account/Migrations/20250221121952_RemovedUserFields.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Account.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedUserFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppartmentNumber",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DiagnosisCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "HouseNumber",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NameOfDiagnosis",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Policy",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserPatronymic",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserSurname",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AppartmentNumber",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<int>(
+                name: "DiagnosisCode",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "HouseNumber",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameOfDiagnosis",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Policy",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserPatronymic",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserSurname",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- Account/Model/User.cs
+++ Account/Model/User.cs
@@ -4,17 +4,5 @@ namespace Account.Model
 {
     public class User : IdentityUser
     {
-        public string UserSurname { get; set; } = string.Empty;
-        public string UserPatronymic { get; set; } = string.Empty;
-        public DateOnly DateOfBirth { get; set; }
-        public string Policy { get; set; } = string.Empty;
-        public string Country {  get; set; } = string.Empty;
-        public string Region {  get; set; } = string.Empty;
-        public string City { get; set; } = string.Empty;
-        public string Street {  get; set; } = string.Empty;
-        public int HouseNumber {  get; set; }
-        public int AppartmentNumber { get; set; }
-        public int DiagnosisCode {  get; set; }
-        public string NameOfDiagnosis { get; set; } = string.Empty;
     }
 }
